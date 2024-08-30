644c828aa17aa0e9ab94dfbe9f79c2baf3045f2f
Aju Abraham <ajubabraham23@gmail.com>
2024-08-30 15:46:29 +0530

    Insert Chartjs

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,24 @@
+import BarChat from "@/components/custom/charts/BarChat"
+import CircleChart from "@/components/custom/charts/CircleChart"
+import LChart from "@/components/custom/charts/LChart"
+
 
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className="rounded-2xl flex gap-8 h-full border-2 p-4 ">
+      <div className="flex flex-col gap-14">
+      <LChart/>
+      <BarChat/>
+      </div>
+      <div className="flex flex-col gap-12">
+      <CircleChart/>
+      <div className="border-2 rounded-xl w-[400px] h-[200px] p-6">
+        Total
+      </div>
+      </div>
+
+
     </div>
   )
 }
