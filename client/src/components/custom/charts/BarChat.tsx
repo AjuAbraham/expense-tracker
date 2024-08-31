62ace6df1e769e79e5393fa1446405eea35b446b
unknown <chiragsharma20202@gmail.com>
2024-08-31 13:18:47 +0530

    chart data

--- client/src/components/custom/charts/BarChat.tsx
+++ client/src/components/custom/charts/BarChat.tsx
@@ -12,22 +12,90 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
-];
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
-  },
-} satisfies ChartConfig;
+import { useContext, useEffect, useState } from "react";
+import { TokenContext } from "@/context/tokenContext";
+import axios from "@/axios";
+
+interface incomeInfo {
+  amount: number;
+  category: string;
+  date: string;
+  description?: string;
+  title: string;
+  _id: string;
+}
 
 const BarChat = () => {
+  const { getToken } = useContext(TokenContext);
+  const [expenses, setExpenses] = useState<[incomeInfo] | []>([]);
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      const token = getToken();
+
+      const res = await axios.get("/expenses/getExpense", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setExpenses(res.data.expense);
+    };
+
+    handleFetch();
+  }, [getToken]);
+
+  const chartData = [];
+  const currentMonth = new Date().getMonth();
+
+  const filteredExpenses = expenses.filter((expense) => {
+    const expenseYear = new Date(expense.date).getFullYear();
+    return expenseYear === 2024;
+  });
+
+  // Group expenses by month and calculate total for each month
+  const monthlyExpenses = filteredExpenses.reduce(
+    (acc: { [key: number]: number }, expense) => {
+      const expenseMonth = new Date(expense.date).getMonth(); // 0 for January, 11 for December
+
+      acc[expenseMonth] = (acc[expenseMonth] || 0) + expense.amount;
+      return acc;
+    },
+    {}
+  );
+
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  for (let i = 0; i <= currentMonth; i++) {
+    const desktopValue = monthlyExpenses[i] || 0;
+
+    chartData.push({
+      month: months[i],
+      desktop: desktopValue,
+    });
+  }
+
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "hsl(var(--chart-1))",
+    },
+  } satisfies ChartConfig;
+
   return (
     <Card className="w-[400px] rounded-xl border-2">
       <CardHeader>
